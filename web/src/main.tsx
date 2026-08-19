9d80e21a41843e69e6b2b3664510f317cc615e8b
Ankit vishwa <aviatankit@gmail.com>
2026-08-20 01:19:22 +0530

    feat: add realtime provider administration

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AuthGate from './AuthGate';
 import './styles.css';
+import './provider.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthGate /></React.StrictMode>);
