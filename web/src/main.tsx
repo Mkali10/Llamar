078edb28f70217af44a4ffdd58b421ec3e909708
Ankit vishwa <aviatankit@gmail.com>
2026-08-20 02:34:10 +0530

    Make call flow builder operational: update web/src/main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -10,5 +10,6 @@ import './live.css';
 import './telephony.css';
 import './team.css';
 import './dids.css';
+import './flow.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthGate /></React.StrictMode>);
