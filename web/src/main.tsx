d7e2811544b7bd66074160b70d3553cb25a97534
Ankit vishwa <aviatankit@gmail.com>
2026-08-20 01:31:27 +0530

    Add multi-agent routing: update web/src/main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,5 +4,6 @@ import AuthGate from './AuthGate';
 import './styles.css';
 import './provider.css';
 import './studio.css';
+import './routing.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AuthGate /></React.StrictMode>);
